ff017a5e015ed9c65c80129ae5ed1ddcaf8120a0
John Recny <jrecny@outlook.com>
2020-01-06 13:38:29 -0500

    add quarter time red circle

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
 const circle = document.querySelector('circle');
 
-const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+let perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
 
 let dur;
+let originalTime;
 const timer = new Timer(duration, startBtn, pauseBtn, {
 	onStart (totalDuration) {
+		originalTime = duration.value;
 		dur = totalDuration;
 	},
 	onTick (timeRemaining) {
-		circle.setAttribute('stroke-dashoffset', perimeter * timeRemaining / dur - perimeter);
+		const perm = perimeter * timeRemaining / dur - perimeter;
+		if (duration.value < originalTime / 4) {
+			circle.setAttribute('stroke', 'red');
+		}
+		circle.setAttribute('stroke-dashoffset', perm);
 	},
 	onComplete () {
 		console.log('timer is completed');
